e98dcc9d23eaea7f7a62d66feb458d7e54d3439f
kushal-modi <cuteromantickittu@gmail.com>
2021-04-14 01:01:48 +0530

    don't know

---
+++ YoneRobot/modules/nightmodebtn.py
@@ -0,0 +1,8 @@
+__help__ = """
+Close your group at night
+ ❍ /nightmode [on/off]: turns on/off nightmode
+ ❍ /setnightmode [zone|closing time|opening time]: sets the time details
+Syntax: /setnightmode America/New_York | 12:00:00 PM | 07:00:00 AM
+"""
+
+__mod_name__ = "Night Mode"

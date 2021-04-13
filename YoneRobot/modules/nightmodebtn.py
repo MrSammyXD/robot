ecdbdf43c7992859f7024bb7f8c7965d66c3edd2
kushal-modi <cuteromantickittu@gmail.com>
2021-04-14 01:10:33 +0530

    oops

--- YoneRobot/modules/nightmodebtn.py
+++ YoneRobot/modules/nightmodebtn.py
@@ -1,8 +1,10 @@
+__mod_name__ = "NightMode"
+
 __help__ = """
 Close your group at night
  ❍ /nightmode [on/off]: turns on/off nightmode
  ❍ /setnightmode [zone|closing time|opening time]: sets the time details
 Syntax: /setnightmode America/New_York | 12:00:00 PM | 07:00:00 AM
+
 """
 
-__mod_name__ = "Night Mode"
--- YoneRobot/mongo.py
+++ YoneRobot/mongo.py
@@ -2,7 +2,7 @@
 import asyncio
 import sys
 
-# from motor import motor_asyncio
+from motor import motor_asyncio
 from YoneRobot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
@@ -16,10 +16,10 @@ MONGO_DB = "DaisyX"
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
-# motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI)
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
+db = motor[MONGO_DB]
 db = client["yonerobot"]
-# db = motor[MONGO_DB_URI]
-# try:
-#     asyncio.get_event_loop().run_until_complete(motor.server_info())
-# except ServerSelectionTimeoutError:
-#     sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
+try:
+    asyncio.get_event_loop().run_until_complete(motor.server_info())
+except ServerSelectionTimeoutError:
+    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))

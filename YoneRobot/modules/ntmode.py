09af141a83920bafb06fc22b4723134bc9d24355
☞丂ﾑﾶﾶƳ☜ <76897487+MrSammyXD@users.noreply.github.com>
2021-04-16 22:22:33 +0530

    Create ntmode.py

---
+++ YoneRobot/modules/ntmode.py
@@ -0,0 +1,10 @@
+__mod_name__ = "Night Mode"
+
+__help__ = """
+Close your group at night
+
+ ❍ /nightmode [on/off]: turns on/off nightmode
+ ❍ /setnightmode [zone|closing time|opening time]: sets the time details
+ ❍ /setnightmode America/New_York | 12:00:00 PM | 07:00:00 AM
+
+"""

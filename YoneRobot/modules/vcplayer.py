e26147af86c93abde05f1beb6a1c891a796475fa
☞丂ﾑﾶﾶƳ☜ <76897487+MrSammyXD@users.noreply.github.com>
2021-04-17 22:13:59 +0530

    Create vcplayer.py

---
+++ YoneRobot/modules/vcplayer.py
@@ -0,0 +1,29 @@
+__mod_name__ = "Voice Player"
+
+__help__ = """
+⭕⭕ *Setting up* ⭕⭕
+
+✅1) Make bot admin
+✅2) Start a voice chat
+✅3) Try /play [song name] for the first time by an admin
+*) If userbot joined enjoy music, If not add @InnexiaHelper to your group and retry
+
+❇ *COMMANDS* ❇
+
+ /saavn To search song on jio saavan and play the first result 
+ /deezer To search the song on deezer and get good quality stream 
+ /ytt To search the song on Youtube and play the first matching result 
+ '/play Reply this in response to a link or any telegram audio file it will be played 
+
+⏯ *PlayBack* ⏯
+ /skip to skip current song 
+ /stop or /end to stop the streaming of song 
+ /pause to pause the stream 
+ /resume to resume the playback. 
+ Inline search is also supported.
+
+❇ * More Tools* ❇
+/admincache: Updates admin info of your group. Try if bot isn't recognize admin💹💹
+
+🔴 *PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME*🔴
+"""
